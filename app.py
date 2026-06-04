a68f8c554066680cfda46ff3df8220a7c4deb12f
oxo-ops <takahashiparu121@gmail.com>
2026-06-04 18:42:37 +0900

    add github pages index

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "DKSS 起動成功"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
